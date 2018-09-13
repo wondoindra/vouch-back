e1c1f57aa778707d0b2cdb23ae4006608b1b9377
Wondo Indra <wondoindra28@gmail.com>
2018-09-14 01:24:06 +0700

    edited port and deploy heroku

--- index.js
+++ index.js
@@ -5,36 +5,16 @@ const bodyparser = require("body-parser");
 const dbconfig = require("./config/dbconfig");
 const ticketroute = require("./routes/ticketroute");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(dbconfig.url);
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: ["*"],
-//     methods: ["GET", "HEAD", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
-//     credentials: true
-//   })
-// );
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 ticketroute(app);
 
-app.listen(8080);
-console.log("app listening on port 8080");
+app.listen(PORT);
+console.log(`app listening on port ${PORT}`);
